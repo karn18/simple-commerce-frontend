8332ce5871fc05a6d05fd18f4c2599c5344de4eb
Karn Tirasoontorn <karn@hey.com>
2026-08-28 06:37:28 +0700

    fix: grow empty cart

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -7,7 +7,11 @@ export default function Cart() {
     <div className="flex min-h-screen flex-col items-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex w-full max-w-3xl flex-1 flex-col gap-4 bg-white px-4 dark:bg-black sm:px-8 mx-auto">
         <Navbar />
-        <MyCart />
+
+        <section className="grow shrink">
+          <MyCart />
+        </section>
+
         <Footer />
       </main>
     </div>
